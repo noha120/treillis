d073185ee665e6d29a22f9f65cbde25754568311
HibaAglou <haglou4142@gmail.com>
2022-05-26 14:09:52 +0200

    feat(build): renamed App as Main & updated dependencies

--- src/main/java/fr/insa/groupe1/treillis/gui/Main.java
+++ src/main/java/fr/insa/groupe1/treillis/gui/Main.java
@@ -2,15 +2,13 @@ package fr.insa.groupe1.treillis.gui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
